74d2890a2d1cddafad0e496ea5d83865997b6a51
saikrishna1426 <68234255+saikrishna1426@users.noreply.github.com>
2020-07-14 08:22:52 -0700

    Add files via upload

---
+++ contactform.py
@@ -0,0 +1,17 @@
+print('enter contact form  details')
+name = input("please enter your name:")
+age = input("age:")
+age= int(age)
+course = input("course:")
+contact = int(input("contact number:"))
+print("  contact form ")
+print("name :",name)
+print("age :",age)
+print("course :",course)
+print("contact no. :",contact)
+
+x= input("please check the information you entered is {right} or { wrong}")
+print(x)
+
+
+
